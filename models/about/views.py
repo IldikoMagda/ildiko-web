bda28ba499b1e52ee628d7f2a079787f885f6fd2
IldikoMagda <ildiko.magda@gmail.com>
2019-09-19 12:32:09 +0100

    sticky navbar, directing to different routes, pyc files deleted

--- models/about/views.py
+++ models/about/views.py
@@ -2,9 +2,7 @@ from flask import Blueprint, render_template
 
 __author__ = 'Ildiko'
 
-
-about_blueprint = Blueprint('about', __name__)
-
+about_blueprint = Blueprint('about', __name__,template_folder='templates')
 
 @about_blueprint.route('/', methods=['GET'])
 def index(): 
