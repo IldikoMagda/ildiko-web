8db49bfa694f1ca7573062b2419fd1d281b8ec05
IldikoMagda <ildiko.magda@gmail.com>
2019-10-17 10:28:56 +0100

    fifth section media screen nicer, added facts to display. css adjustments

--- static/facts.js
+++ static/facts.js
@@ -10,7 +10,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every whatever seconds USE ODD NUMBER OF FACTS
 }
 
 showSlides();
