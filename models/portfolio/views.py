dfd4ff80fd5f458fffc75bc5e0a2677b160871bd
IldikoMagda <ildiko.magda@gmail.com>
2020-03-29 17:29:39 +0100

    Routes to each project created, each html created and linked to portfolio.index

--- models/portfolio/views.py
+++ models/portfolio/views.py
@@ -10,3 +10,23 @@ portfolio_blueprint = Blueprint('portfolio', __name__)
 def index(): 
 
     return render_template('portfolio/index.html')
+
+@portfolio_blueprint.route('/MachineLearning', methods=['GET'])
+def machinelearning(): 
+
+    return render_template('portfolio/machinelearning.html')
+
+@portfolio_blueprint.route('/WebApp', methods=['GET'])
+def webapp(): 
+
+    return render_template('portfolio/webapp.html')
+
+@portfolio_blueprint.route('/Website', methods=['GET'])
+def website(): 
+
+    return render_template('portfolio/website.html')
+
+@portfolio_blueprint.route('/ProstateCancerResearch', methods=['GET'])
+def cancerresearch(): 
+
+    return render_template('portfolio/cancerresearch.html')
