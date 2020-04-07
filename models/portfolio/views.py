ee82e7bf524b61122dd02c1b0248f96e7c72f4ce
IldikoMagda <ildiko.magda@gmail.com>
2020-04-07 15:41:01 +0100

    first try for download dissertation file under portfolio/machinelearning

--- models/portfolio/views.py
+++ models/portfolio/views.py
@@ -1,17 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file, send_from_directory
 
 __author__ = 'Ildiko'
 
+import config
+import os
 
 portfolio_blueprint = Blueprint('portfolio', __name__)
 
+FOLDER = config.STATIC_FOLDER
 
 @portfolio_blueprint.route('/', methods=['GET'])
 def index(): 
 
     return render_template('portfolio/index.html')
 
-@portfolio_blueprint.route('/MachineLearning', methods=['GET'])
+@portfolio_blueprint.route('/MachineLearning', methods=['GET','POST'])
 def machinelearning(): 
 
     return render_template('portfolio/machinelearning.html')
@@ -20,6 +23,24 @@ def machinelearning():
 def webapp(): 
 
     return render_template('portfolio/webapp.html')
+
+@portfolio_blueprint.route('portfolio/download', methods=['GET'])
+def download():
+    try:
+        root_dir = os.path.dirname(os.getcwd())
+        path = os.path.join(root_dir, 'ildiko-web', 'static')
+        print(path)
+        print(root_dir)
+        return send_from_directory(path, filename='dissertation.docx')
+
+        #file= os.path.join(FOLDER, 'dissertation.docx')
+        #return send_file(file, attachment_filename="IldikoMagdaDissertation")
+        #return send_file('FutureDissertationnewresults.docx',mimetype='docx',as_attachment=True,attachment_filename='IldikoMagdaBioinf')
+        #return send_from_directory('dissertation.docx', FOLDER, as_attachement=True)
+        #return print(file)
+    
+    except Exception as e:
+        return str(e, root_dir, path)
 
 @portfolio_blueprint.route('/Website', methods=['GET'])
 def website(): 
