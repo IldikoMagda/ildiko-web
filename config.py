0443cac338a80db59b2d9d98aca6e51bc6db85a4
IldikoMagda <ildiko.magda@gmail.com>
2022-08-07 09:46:04 +0100

    adding database on heroku site, setting up config file locally

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 #config.py
-
 import os
 DEBUG = True    # Turns on debugging features in Flask
 RELOADER = True
-
-
 SECRET_KEY = 'development key'
 STATIC_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/static'
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
