d52b752d0afe2e8808c785de1b192e84e99ebc9d
IldikoMagda <ildiko.magda@gmail.com>
2022-08-07 16:17:45 +0100

    Torturous fixing of web app crash whilst not even fixing db conn.

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
 ###app.py ###
 from flask import Flask, render_template
-from sqlalchemy import create_engine
-import psycopg2
 
 from models.about.views import about_blueprint
 #from models.interactive.views import interactive_blueprint
 from models.portfolio.views import portfolio_blueprint
 from models.trader.views import trader_blueprint
 
-#import on flask mail doesn't work
-#from models.contact.views import contact_blueprint
-#from flask_mail import Message, Mail
-
 #mail = Mail()
 app = Flask(__name__)
 app.config.from_object('config')
-DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
-db = create_engine(DB_URI)
-db.execute("CREATE TABLE IF NOT EXIST stocks (stock_name text, stock_price_purchase int, currentprice int)")
-
-db.execute("INSERT INTO stocks(stock_name, stock_price_purchase, current_price) VALUES('TWTR', 38.23, 42.52)")
-
 
 @app.route("/")
 def home():
